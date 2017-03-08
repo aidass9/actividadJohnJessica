4fa05b5cde72c8d9a1397b0e9f1b30a846672411
jllorca <jllorca@INF2-PC16>
2017-03-08 21:07:38 +0100

    Segundo commit

--- Hola mundo/index.php
+++ Hola mundo/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            // put your code here
+            echo "Jorge Llorca Sendra y Aida Soriano Serra";
         ?>
     </body>
 </html>
